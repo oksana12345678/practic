0526e072acbe14c61afd1068642b415e1ca668ae
oksana12345678 <oksana.tanashuk@gmail.com>
2024-01-12 19:46:10 +0100

    add

---
+++ test.js
@@ -0,0 +1,10 @@
+let username = 'Poly';
+username = 'Harambe';
+username = 'Billy';
+username = 'Joe';
+let message = `Username ${'Poly'} is ${'Poly'.length} characters long. Username ${'Harambe'} is ${
+  'Harambe'.length
+} characters long. Username ${'Billy'} is ${'Billy'.length} characters long .Username ${'Joe'} is ${
+  'Joe'.length
+} characters long`;
+console.log(message);
